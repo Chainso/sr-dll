f4fa69a142c2c0e1bb8dfd8e15d52008f5676158
unknown <chanzobryan@gmail.com>
2021-12-16 19:48:00 -0500

    Adding named pipes for I/O

--- SpeedRunnersAI/include/inject/server.h
+++ SpeedRunnersAI/include/inject/server.h
@@ -2,6 +2,9 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+#define WRITE_BUFFER_SIZE 256
+#define READ_BUFFER_SIZE 8
+
 int GameServer();
 
 #endif // SERVER_H
--- SpeedRunnersAI/src/inject/server.cpp
+++ SpeedRunnersAI/src/inject/server.cpp
@@ -8,7 +8,60 @@
 #include <fstream>
 
 /**
+ * @brief Creates a packet from the current game state.
+ * 
+ * @param game  The game state to create the packet from.
+ * @returns     The create packet as a string
+ */
+static std::string CreatePacket(Game* game)
+{
+
+}
+
+/**
  * @brief The main game server loop.
+ * 
+ * @param game  The current running game.
+ * @return      0 on success, -1 otherwise.
+ */
+static int GameServerLoop(Game* game)
+{
+    // Create named pipe to use
+    LPCWSTR pipe_name = L"SpeedRunnersAI-dll";
+    
+    HANDLE pipe = CreateNamedPipe(pipe_name, PIPE_ACCESS_DUPLEX, PIPE_TYPE_MESSAGE | PIPE_REJECT_REMOTE_CLIENTS, 1, WRITE_BUFFER_SIZE, READ_BUFFER_SIZE, 0, NULL);
+
+    if (pipe == INVALID_HANDLE_VALUE)
+    {
+        return -1;
+    }
+
+    char read_buffer[READ_BUFFER_SIZE];
+    DWORD bytes_read;
+
+    char write_buffer[WRITE_BUFFER_SIZE];
+    DWORD bytes_written;
+
+    while (true)
+    {
+        // Read cycle
+        if (PeekNamedPipe(pipe, read_buffer, READ_BUFFER_SIZE, &bytes_read, NULL, NULL))
+        {
+            // Data is available
+            if (!ReadFile(pipe, read_buffer, READ_BUFFER_SIZE, &bytes_read, NULL))
+            {
+                // Error reading
+                return -1;
+            }
+        }
+
+        // Write cycle
+        //WriteFile(pipe, write_buffer, -1, &bytes_written, NULL);
+    }
+}
+
+/**
+ * @brief Starts the game server to receive inputs and export packets.
  * 
  * @return int  The return status of the server, -1 on error.
  */
@@ -26,7 +79,6 @@ int GameServer()
 
     CloseHandle(process);
     CloseHandle(thread);
-
 
     std::ofstream fs("C:\\Users\\Chainso\\Desktop\\zz.txt");
 
